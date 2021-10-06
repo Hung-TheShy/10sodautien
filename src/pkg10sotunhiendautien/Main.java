331d7539bf0907761d5c36e588004807dd431f83
DELL <DELL@DESKTOP-OGRVV23>
2021-10-06 14:23:58 +0700

    10sodautien

---
+++ src/pkg10sotunhiendautien/Main.java
@@ -0,0 +1,15 @@
+
+package pkg10sotunhiendautien;
+
+
+public class Main {
+
+   
+    public static void main(String[] args) {
+        for(int i =0;i<10;i++)
+        {
+            System.out.println(i);
+        }
+    }
+    
+}
